9a4fdda476936c468a45808ffd86d89b4622302e
Mohammad Luqman <moluqman@moluqman-macbookpro.roam.corp.google.com>
2020-04-12 14:11:41 +0530

    Limit the results to top 20 sites

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 "use strict";
+var MAX_DOMAINS_SHOWN=20;
 
 // This function is called as soon as https://apis.google.com/js/client.js
 // finishes loading.
@@ -10,18 +11,30 @@ var isDomainsSame = function(userProvidedHostname, urlHostname) {
   return false; 
 };
 
-function onLoad() {
-  var usageStats = JSON.parse(localStorage.getItem("gwatchme_data"));
-  var usageSettings = JSON.parse(localStorage.getItem("gwatchme_settings"));
+function _compareDurations(duration1, duration2) {
+  return -(duration1.duration - duration2.duration); 
+}
+
+function getTopKDomains(durations) {
+  var domainsToShow = Math.min(MAX_DOMAINS_SHOWN, durations.length); 
+  durations = durations.sort(_compareDurations); 
+  durations = durations.slice(0,domainsToShow);
+  return durations; 
+}
+
+function displaySiteList(usageSettings, usageStats) {
+  // usageStats=[{"domain":"x.com","duration":1000},{"domain":"y.com","duration":200}]
+  // usageSettings=[{"website_name":"x.com","website_duration":4},{...}]
   if (usageStats) {
     var result="<tr><th>Website</th><th>Time Limit</th><th>"+
     "Time Spent </th></tr>";
     for (var i in usageStats){
-        result+='<tr  align="center">';
+        var domain = usageStats[i].domain;
+        result+='<tr  align="left">';
         var timeLimit = "N/A";
         if (usageSettings){
             for (var j in usageSettings){
-                if (isDomainsSame(usageSettings[j].website_name,i)) {
+                if (isDomainsSame(usageSettings[j].website_name,domain)) {
                   timeLimit = usageSettings[j].website_duration;
                 }
             }
@@ -29,7 +42,8 @@ function onLoad() {
         if(timeLimit != "N/A") {
           timeLimit = timeLimit + " minutes";
         }
-        result+="<td>"+i+"</td>"+"<td>"+timeLimit+"</td>"+"<td>"+Math.round(usageStats[i]/60)+" minutes </td></tr>";
+        var timeSpent = Math.round((usageStats[i].duration)/60);
+        result+="<td>"+domain+"</td>"+"<td>"+timeLimit+"</td>"+"<td>"+timeSpent+" minutes </td></tr>";
     }
     $('site-list').innerHTML = result;
   }
@@ -42,48 +56,65 @@ function openOptionsPage()
   chrome.runtime.openOptionsPage();
 }
 
-function displaySiteList(siteList){
+function logSiteList(siteList){
   for (const i in siteList){
     console.log(i);
   }
+}
+
+//Function plots topKdomains
+function plotPieChart(durations) {
+
+  var durationsdata = []
+  var domainNames = []
+
+  for (var index in durations) {
+    durationsdata.push(durations[index].duration); 
+    domainNames.push(durations[index].domain);
+  }
+
+  //Plot chart
+  var ctx = $('donut-canvas').getContext("2d");
+  var data = {
+      datasets: [{
+          data: durationsdata,
+          backgroundColor: ["#29B6F6","#26C6DA","#26A69A","#66BB6A","#9CCC65","#D4E157","#FFEE58","#FFCA28","#FFA726","#FF7043","#EF9A9A","F48FB1","#90CAF9","#81D4FA","#80DEEA","#80CBC4","#A5D6A7","#C5E1A5","#E6EE9C","#FFF59D","#EF5350","#EC407A","#AB47BC","#7E57C2","#5C6BC0","#42A5F5"]
+
+      }],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: domainNames
+  };
+
+  var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: {
+          legend: {
+            display: false
+          }
+        }
+    });
 }
 
 var bkg = chrome.extension.getBackgroundPage();
 
 const $ = id => document.getElementById(id);
 
-onLoad()
 
 window.onload = function() {
 
-      var usageStats = JSON.parse(localStorage.getItem("gwatchme_data"));
-      var ctx = $('donut-canvas').getContext("2d");
-      var durations = []
-      var domainNames = []
-      for(var key in usageStats) {
-        durations.push(usageStats[key]);
-        domainNames.push(key); 
-      }
-      var data = {
-          datasets: [{
-              data: durations,
-              backgroundColor: ["#EF5350", "#EC407A", "#AB47BC", "#7E57C2", "#5C6BC0", "#42A5F5", "#29B6F6", "#26C6DA", "#26A69A", "#66BB6A", "#9CCC65", "#D4E157", "#FFEE58", "#FFCA28", "#FFA726", "#FF7043", "#EF9A9A", "F48FB1", "#90CAF9", "#81D4FA", "#80DEEA", "#80CBC4", "#A5D6A7", "#C5E1A5", "#E6EE9C", "#FFF59D"]
+  var usageStats = JSON.parse(localStorage.getItem("gwatchme_data"));
+  var usageSettings = JSON.parse(localStorage.getItem("gwatchme_settings"));
+  var durations = []
+  for(var key in usageStats) {
+    durations.push({domain: key, duration:usageStats[key]});
+    //domainNames.push(key); 
+  }
+  //Trim the data
+  durations = getTopKDomains(durations);
 
-          }],
-
-          // These labels appear in the legend and in the tooltips when hovering different arcs
-          labels: domainNames
-      };
-
-    var myDoughnutChart = new Chart(ctx, {
-          type: 'doughnut',
-          data: data,
-          options: {
-            legend: {
-              display: false
-            }
-          }
-      });
-
+  displaySiteList(usageSettings, durations);
+  plotPieChart(durations);
 
 };
